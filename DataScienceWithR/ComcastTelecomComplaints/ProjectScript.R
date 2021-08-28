44aa598d97d0ecdadc2019270fe93f0da17dc8da
Nonyane, Luke <nonyane18@gmail.com>
2021-08-28 10:03:29 -0400

    completed script and assignment

--- DataScienceWithR/ComcastTelecomComplaints/ProjectScript.R
+++ DataScienceWithR/ComcastTelecomComplaints/ProjectScript.R
@@ -137,10 +137,118 @@ plyr::count(complaint_types) # These are the distinct complaint types; 1841
 complaint_types_table <- plyr::count(complaint_types)
 View(complaint_types_table)
 
+############################################################################
 
+# Create a new categorical variable with value as Open and Closed. 
+# Open & Pending is to be categorized as Open and Closed & Solved is to be categorized as Closed.
+trial_data = read.csv("Comcast Telecom Complaints data1.csv", sep=",")
+trial_df <- tibble(trial_data)
+head(trial_df)
 
-# Set complaint_years to factor (effectively binning)
-#complaint_years <- year(v1)
-#complaint_years
+install.packages("dplyr")
+library(dplyr)
 
+trial_df <- trial_df %>%
+  mutate(condensed_status = case_when(Status == 'Open' | Status == 'Pending' ~ 'Open',
+                                      Status == 'Solved' | Status == 'Closed' ~ 'Closed'))
+
+View(trial_df)
+
+############################################################################
+
+## Provide state wise status of complaints in a stacked bar chart. Use the categorized variable from Q3##
+
+# How many different states do we have? 
+sapply(customer_complaints$State, factor)
+
+# Colors for the legend
+  # showCols(bg="gray20",cl=colors()[1:60], rot=30, cex=0.9)
+  # showCols(cl= colors(), bg="gray33", rot=30, cex=0.75)        
+  # View(showCols(cl= colors(), bg="gray33", rot=30, cex=0.75))
+  # colors <- showCols(cl = colors(), bg = "gray33", rot = 30, cex = 0.75)[["label"]]
+
+statewise_complaints <- table(trial_df$State,trial_df$condensed_status)
+
+barplot(statewise_complaints,
+        main="Statewise complaints",
+        col=c("grey", "cornflowerblue", "aliceblue", "bisque2", "blueviolet", "brown4", "burlywood4", "cadetblue4",
+              "chartreuse4", "chocolate4", "coral4", "cornsilk3", "cyan3", "darkgoldenrod1", "darkgreen", "darkolivegreen1",
+              "darkorange1", "darkorchid1", "darksalmon", "darkseagreen4", "darkslategray3", "deeppink", "deepskyblue",
+              "dimgray", "dodgerblue3", "firebrick3", "gold4", "goldenrod4", "gray3", "honeydew4", "hotpink4", "indianred4",
+              "ivory4", "khaki4", "lavenderblush3", "lemonchiffon2", "lightblue2", "lightcyan1", "lightgoldenrod1",
+              "lightyellow3", "magenta1", "maroon1", "mediumblue", "mediumorchid4"),
+        legend.text = TRUE,
+        args.legend = list(x = "topright",
+                           inset = c(-0.009, -0.1),
+                           cex=0.265))
+
+############################################################################
+
+# Which state has the maximum complaints
+open_complaints <- statewise_complaints[,"Open"]
+closed_complaints <- statewise_complaints[,"Closed"]
+View(open_complaints)
+View(closed_complaints)
+states <- unique(trial_df$State)
+
+# For loop to sum up each of the states' open + closed tickets
+i <- 1
+for (i in states) {
+  print(i)
+  print(as.numeric(open_complaints[i]) + as.numeric(closed_complaints[i]))
+}
+
+# The state of Georgia has the maximum complaints
+
+############################################################################
+
+## Which state has the highest percentage of unresolved complaints ##
+ 
+# For loop to find the percentages of unresolved complaints
+e <- 1
+for (e in states) {
+  print(e)
+  print("Total complaints")
+  print(as.numeric(open_complaints[e]) + as.numeric(closed_complaints[e]))
+  print("Percent of unresolved complaints:")
+  print( as.numeric(open_complaints[e]) / (as.numeric(open_complaints[e]) + as.numeric(closed_complaints[e])))
+}
+
+# The state of Kentucky has the highest percentage of unresolved complaints at 0.4285714 ~ 43%
+
+############################################################################
+## Provide the percentage of complaints resolved till date, which were received through the Internet and customer care calls. ##
+
+# Use trial_df columns condensed_status & received.via
+# Collect all complaints received through Internet & Customer care calls
+
+calls_received_through_internet_and_ccarecalls <- table(trial_df$Received.Via, trial_df$condensed_status)
+View(calls_received_through_internet_and_ccarecalls)
+
+# From the view above we can see the closed tickets for customer care calls = 864 and internet = 843
+
+calls_received_through_internet_and_ccarecalls[,"Closed"]
+ccarecalls_internet_closed <- calls_received_through_internet_and_ccarecalls[,"Closed"]
+ccarecalls_internet_open <- calls_received_through_internet_and_ccarecalls[,"Open"]
+
+ccarecall_closed <- ccarecalls_internet_closed["Customer Care Call"]
+ccarecall_closed
+internet_closed <- ccarecalls_internet_closed["Internet"]
+internet_closed
+
+ccarecall_open <- ccarecalls_internet_open["Customer Care Call"]
+ccarecall_open
+internet_open <- ccarecalls_internet_open["Internet"]
+internet_open
+
+total_tickets <- as.numeric(ccarecall_closed) + as.numeric(internet_closed) + as.numeric(ccarecall_open) + as.numeric(internet_open)
+total_tickets
+
+total_resolved_through_ccarecall_internet <- as.numeric(ccarecall_closed) + as.numeric(internet_closed)
+total_resolved_through_ccarecall_internet
+
+percentage_resolved_through_ccarecall_internet <- total_resolved_through_ccarecall_internet / total_tickets
+percentage_resolved_through_ccarecall_internet
+
+############################################################################
 
